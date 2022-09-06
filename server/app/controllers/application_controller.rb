227647996da578eb06ca599ded7fe4e7f666e0b4
Tonny Oseko <tonnyoseko0@gmail.com>
2022-09-07 01:57:35 +0300

    add endpoints for products, product_by_id

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
     
-    # Add your routes here
+    # homepage
     get "/" do
-      { message: "Good luck with your project!" }.to_json
+      "welcome to the homepage!!"
+    end
+
+    # default route products
+    get "/products" do
+      products = Product.all
+      products.to_json
+    end
+
+    # get product by id
+    get "/products/:id" do
+      products = Product.find(params[:id])
+      products.to_json
     end
   
 end

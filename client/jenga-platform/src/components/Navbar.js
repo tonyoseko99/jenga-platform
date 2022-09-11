21b30f59e76a166c04a5dcef557373477a12fcbe
Tonny Oseko <tonnyoseko0@gmail.com>
2022-09-11 23:30:28 +0300

    add styling and navlinks to the navbar

--- client/jenga-platform/src/components/Navbar.js
+++ client/jenga-platform/src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { FaCartArrowDown } from "react-icons/fa";
 import Home from "./Home";
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="main">
       <nav className="navbar">
-        <h1>Jenga Shop</h1>
-        <a href="/carts"><FaCartArrowDown /> Cart</a>
-        <a className="login-form" href="/login"><button>Login</button></a>
+          <Link to="/"><h1>Jenga Shop</h1></Link>
+        <nav className="nav-items">
+          <Link to="/carts"><FaCartArrowDown /> Cart</Link>
+          <Link to="/login"><button>Login</button></Link>
+          <Link to="/login"><button>Signup</button></Link>
+        </nav>
       </nav>
     </div>
   );
 }
 export default Navbar;
+
+{/* <a href="/carts"><FaCartArrowDown /> Cart</a>
+        <a className="login-form" href="/login"><button>Login</button></a> */}
